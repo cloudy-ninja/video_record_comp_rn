0a2f5e21bff5cdef9e271b22ba544bed1dc10407
cloudy-ninja <msr0327@outlook.com>
2019-07-20 01:04:06 +0800

    add android permission dialog

--- screen/Home.js
+++ screen/Home.js
@@ -99,6 +99,12 @@ export class HomeScreen extends React.Component {
           }}
           type={RNCamera.Constants.Type.front}
           style={style.previewCamera}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
         />
         <Preview
           recordingTime={currentTime}
